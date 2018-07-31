521a75ef36fa5890a180b316fae630b179d6006e
Hussein Barada <hsein.barada@gmail.com>
2018-07-31 16:41:46 +0300

    The player can now move in 4 different directions

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//'use strict';
 // Enemies our player must avoid
 var Enemy = function(x, y, speed, name) {
     // Variables applied to each of our instances go here,
@@ -40,24 +41,132 @@ Enemy.prototype.render = function() {
 // a handleInput() method.
 var Player = function(x, y) {
     this.sprite = 'images/char-boy.png';
-    this.x = x;
-    this.y = y;
+    this.x = 200;
+    this.y = 400;
+    //this.width = 70;
+    //this.height = 94;
+    this.health = 100;
+    this.damaged = false;
 };
 
 Player.prototype.update = function(dt) {
-    
+    if (pause) {
+        this.isTakingDamage = false;
+        return
+    }
+
+    /*for(let i = 0; i < allEnemies.length; i += 1) {
+        this.handleCollision(allEnemies[i]);
+        if (this.damaged) {
+            return;
+        }
+    } */   
 };
+
+/*Player.prototype.handleCollision = function(enemy) {
+    function pointInRect(point, rect){
+        var x = point.x;
+        var y = point.y;
+        var x1 = rect.x;
+        var y1 = rect.y;
+        var x2 = rect.x + rect.width;
+        var y2 = rect.y + rect.height;
+        
+        return x1 <= x && x <= x2 && y1 <= y && y <= y2 ;
+      
+      }
+    
+      function rectOverlap(rect1,rect2){
+
+        var c1 = {"x": rect1.x,               "y": rect1.y};      // top left
+        var c2 = {"x": rect1.x + rect1.width, "y": rect1.y};      // top right
+        var c3 = {"x": rect1.x ,              "y": rect1.y + rect1.height}; // bottom left
+        var c4 = {"x": rect1.x + rect1.width, "y": rect1.y + rect1.height}; // bottom right
+      
+        return (pointInRect(c1, rect2) ||
+        pointInRect(c2, rect2) ||
+        pointInRect(c3, rect2) ||
+        pointInRect(c4, rect2));
+      }
+      //if there is an overlap of player and enemy the player takes damage
+      let isTakingDamage = rectOverlap(player, enemy) || rectOverlap(enemy, player); 
+      if (isTakingDamage){
+        //   console.log(enemy.name);
+          this.sprite = 'images/char-cat-girl-damage.png';
+          this.damaged = true;
+          this.health -= 1;
+        //   console.log(player.health);
+          if (this.health < 0) {
+            //   console.log("game over");
+              reset();
+          }
+          
+      } else {
+          this.sprite = 'images/char-cat-girl.png';
+          this.damaged = false;
+      }
+      
+}*/
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+//handleInput method recieves the event (e) detected by listener
+Player.prototype.handleInput = function(e){
+ if (pause) {
+    switch(e){
+        case "pause":
+        pause = !pause;  //toggles between pause and resume
+        console.log( (pause ? "pause" : "resume") );               
+        break;
+    default:
+        return; // do nothing 
+    }
+ } else {
+    switch(e) {
+        case "up":
+            this.y -= 50;
+            break;
+        case "down":
+             this.y += 50;
+            break;
+        case "left":
+            this.x -= 50;
+            break;  
+        case "right":
+            this.x += 50;
+            break;
+        case "pause":
+            pause = !pause;  //toggles between pause and resume
+            console.log( (pause ? "pause" : "resume") );               
+            break;
+        default:
+            return; // do nothing
+    }
+ }
+    //prevents character from moving outside of gameboard
+    if (this.x > 500 || this.x < 0) {
+        this.x = 200;
+    }
+
+    if (this.y > 500 || this.y < 0) {
+        this.y = 400;
+    }
+
+    //detects victory condition
+    if (this.y === 50) {
+        console.log("you reached the water");
+    }
+     
+};
 
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
 var enemyBug1 = new Enemy(0, 100, 100, "E1"); 
 var enemyBug2 = new Enemy(0, 150, 150, "E2");
 var enemyBug3 = new Enemy(0, 200, 75, "E3");
+
+// Place all enemy objects in an array called allEnemies
 var allEnemies = [enemyBug1, enemyBug2, enemyBug3];
 
 // Place the player object in a variable called player
